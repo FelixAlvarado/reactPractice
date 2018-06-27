2255386c7d5e4ace1d57cfee3fb296151891eb66
FelixAlvarado <felixalvarado653@gmail.com>
2018-06-26 17:33:57 -0700

    added client folder

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+const booksRouter = require('./routes/bookRoutes');
+const bodyParser = require('body-parser');
 
-AppRegistry.registerComponent('ReactPractice', () => App);
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+app.use(bodyParser.json());
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/books', booksRouter);
+
+
+require('./routes/authRoutes')(app);
+
+
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
